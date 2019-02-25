32a3dc0369e154365ae44282e5e5e3e41e7040f4
Mira <miralilleholtvik@gmail.com>
2019-02-25 13:23:35 +0100

    First draft, skeleton

---
+++ gan.py
@@ -0,0 +1,33 @@
+def build_discriminator():
+    # Build discriminator
+
+
+def build_generator():
+    # Build generator
+
+
+
+
+
+# How to load data?
+# Can possibly use a built-in generator. 
+
+for epoch in range(epochs):
+    for batch_i, (enhanced, clean) in enumerate("her skal det være en batchdataloader"):
+
+        # Train discriminator
+
+
+        # Train generator 
+
+
+        # Plot progress
+
+
+        # At some intervals, save generated audio
+
+
+
+
+
+
